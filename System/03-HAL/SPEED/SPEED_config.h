847248ae40d57eb879c4e0345ced536a5d172f6b
Adham Eid <adhameid0@gmail.com>
2020-12-28 15:22:12 +0200

    Speed sensor uploaded

---
+++ System/03-HAL/SPEED/SPEED_config.h
@@ -0,0 +1,15 @@
+/**
+ * @file SPEED_config.h
+ * @author Adham Eid (adhameid0@gmail.com)
+ * @brief No configurations needed
+ * @version 1.0
+ * @date 28-12-2020
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#define SPEED_CONFIG_H
+
+
+
+#endif
