2beacbffebb98ff2ea0054f9e14b3208b93e53b2
Ah2m4ed <40804198+Ah2m4ed@users.noreply.github.com>
2021-01-22 22:35:27 +0200

    update of the driver

--- System/03-HAL/USONIC/USONIC_program.c
+++ System/03-HAL/USONIC/USONIC_program.c
@@ -25,75 +25,65 @@ void USONIC_voidInit(){
 	Port_SetPinDirection( USONIC_TRIGGER, OUTPUT_S2MHZ);
 	Port_SetPinMode( USONIC_TRIGGER, GP_OUTPUT_PUSH_PULL );
 	Dio_WriteChannel( USONIC_TRIGGER, 0);
-	//DIO_voidSetPinSpecs (USONIC_TRIGGER,DIO_PIN_OUTPUT_2M_PP);
-	//DIO_voidSetPinValue     (USONIC_TRIGGER, DIO_PIN_VALUE_LOW);
-    //Port_SetPinDirection(USONIC_TRIGGER,OUTPUT_S2MHZ);
-    //Port_SetPinMode(USONIC_TRIGGER,GP_OUTPUT_PUSH_PULL);
-    //Dio_WriteChannel(USONIC_TRIGGER,0);
     //Set the Trigger pin as floating input
 	Port_SetPinDirection( USONIC_ECHO, INPUT);
-	Port_SetPinMode( USONIC_ECHO, INPUT_FLOATING );
-	//DIO_voidSetPinSpecs (USONIC_ECHO, DIO_PIN_INPUT_FLOATING);
-    //Port_SetPinDirection(USONIC_ECHO,INPUT);
-    //Port_SetPinMode(USONIC_ECHO,INPUT_FLOATING);
+	Port_SetPinMode( USONIC_ECHO, INPUT_PULL_DOWN );
     //Set the prescaler of the timer so that the CLK is 1MHZ
     TIMER_voidSetPrescaler(USONIC_TIMER,8);
 }
 
 
 
-f32 USONIC_f32GetDistance(){
+f32 USONIC_f32GetDistance(u8 *DTC_CODE){
     Local_u8Flag=0;
+    u8 Local_u8Counter=0;
     f32 Local_f32Distance=0;
+    u16 Local_u16TimerCount=0;
     Dio_WriteChannel( USONIC_TRIGGER, 1);
-    //DIO_voidSetPinValue(USONIC_TRIGGER,DIO_PIN_VALUE_HIGH);
     TIMER_voidSetBusyWait(USONIC_TIMER,10);
     Dio_WriteChannel( USONIC_TRIGGER, 0);
-    //DIO_voidSetPinValue(USONIC_TRIGGER,DIO_PIN_VALUE_LOW);
     TIMER_voidSetResetTimer(USONIC_TIMER,TIMER_SET,0xFFFF);
-    while (Dio_ReadChannel(USONIC_ECHO)==0);
-    TIMER_voidStartStopCount(USONIC_TIMER ,TIMER_START);
-    while (Dio_ReadChannel(USONIC_ECHO)==1);
-    TIMER_voidStartStopCount(USONIC_TIMER ,TIMER_STOP);
-    Local_f32Distance=(f32)TIMER_u16GetElapsedTime(USONIC_TIMER)/58.82;
-    TIMER_voidSetResetTimer(USONIC_TIMER,TIMER_RESET,0);
+    while (Dio_ReadChannel(USONIC_ECHO)==0&&Local_u8Counter<0xFF)
+    {
+        Local_u8Counter++;
+    }
+    if (Local_u8Counter!=0xFF)
+    {
+        TIMER_voidStartStopCount(USONIC_TIMER ,TIMER_START);
+        while (Dio_ReadChannel(USONIC_ECHO)==1);
+        TIMER_voidStartStopCount(USONIC_TIMER ,TIMER_STOP);
+        Local_u16TimerCount=TIMER_u16GetElapsedTime(USONIC_TIMER);
+        Local_f32Distance=(f32)Local_u16TimerCount/58.82;
+        TIMER_voidSetResetTimer(USONIC_TIMER,TIMER_RESET,0);
+    }
+    
+    
+    if (Local_u8Counter==0xFF)
+    {
+        *DTC_CODE=5;     //ECHO PIN DISCONNECTED
+    }
+    else if (Local_u16TimerCount<1766)    //less than 30 cm   
+    {
+        *DTC_CODE=1;     //RED LED
+    }
+    else if (Local_u16TimerCount<5900)    //less than 100 cm
+    {
+        *DTC_CODE=2;     //ORANGE LED
+    }
+    else if (Local_u16TimerCount<14750)   //less than 250 cm
+    {
+        *DTC_CODE=3;     //GREEN LED
+    }
+    else {
+        *DTC_CODE=4;     //UNCONFIRMED CASE
+    }
+    
+    
     return Local_f32Distance;
 }
+
+
 /*
-void (*Local_CallBackFunc) (f32);
-f32 Local_f32Distance=0;
-
-void USONIC_voidInit(){
-    //Set ECHO pin as 2MHZ output pin and initialy Low
-	DIO_voidSetPinDirection (USONIC_TRIGGER, OUTPUT_GP_2MHZ_PP);
-	DIO_voidSetPinValue     (USONIC_TRIGGER, LOW);
-    //Port_SetPinDirection(USONIC_TRIGGER,OUTPUT_S2MHZ);
-    //Port_SetPinMode(USONIC_TRIGGER,GP_OUTPUT_PUSH_PULL);
-    //Dio_WriteChannel(USONIC_TRIGGER,0);
-    //Set the Trigger pin as floating input
-	DIO_voidSetPinDirection (USONIC_ECHO, INPUT_FLOATING);
-    //Port_SetPinDirection(USONIC_ECHO,INPUT);
-    //Port_SetPinMode(USONIC_ECHO,INPUT_FLOATING);
-    //Set the prescaler of the timer so that the CLK is 1MHZ
-    TIMER_voidSetPrescaler(USONIC_TIMER,USONIC_FREQ);
-}
-
-
-
-f32 USONIC_f32GetDistanceSynch(){
-    DIO_voidSetPinValue(USONIC_TRIGGER,1);
-    TIMER_voidSetBusyWait(USONIC_TIMER,10);
-    DIO_voidSetPinValue(USONIC_TRIGGER,0);
-    TIMER_voidSetResetTimer(USONIC_TIMER,TIMER_SET,0xFFFF);
-    while (DIO_u8GetPinValue(USONIC_ECHO)==0);
-    TIMER_voidStartStopCount(USONIC_TIMER ,TIMER_START);
-    while (DIO_u8GetPinValue(USONIC_ECHO)==1);
-    TIMER_voidStartStopCount(USONIC_TIMER ,TIMER_STOP);
-    Local_f32Distance=(f32)TIMER_u16GetElapsedTime(USONIC_TIMER)/58.82;
-    TIMER_voidSetResetTimer(USONIC_TIMER,TIMER_RESET,0);
-    return Local_f32Distance;
-}
-
 void USONIC_f32GetDistanceAsynch(void (*CallBackFunc) (f32)){
     Local_CallBackFunc=CallBackFunc;
     EXTI_voidSetSignalLatch(EXTI_LINE11,EXTI_FALLING);
