cbae948f5bb5a3694f5dd940248944aaefb5e227
Eslamkh <eslam.kh.kamal@gmail.com>
2021-02-15 00:02:55 +0200

    External Clock Mode 1 is added

--- System/02-MCAL/05-AFIO/AFIO_config.h
+++
@@ -1,9 +0,0 @@
-/**********************************************/
-/* Author   : Eslam Khaled                    */
-/* Date     : 2020-09-03                      */
-/* Version  : V01                             */
-/**********************************************/
-#ifndef AFIO_CONFIG_H
-#define AFIO_CONFIG_H
-
-#endif
