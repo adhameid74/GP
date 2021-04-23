64325116d0f35b14cda20ce4d798b10d2ac12de2
Ah2m4ed <40804198+Ah2m4ed@users.noreply.github.com>
2021-04-23 16:05:00 +0200

    Adding ReadDTCInformation files

---
+++ System/04-SERVICE/UDS Services/ReadDTCInformation/UDS_ReadDTCInformation_interface.h
@@ -0,0 +1,15 @@
+/**
+ * @file UDS_ReadDTCInformation_interface.h
+ * @author Ahmed M.Abdelfattah (ahmedm7md24@gmail.com)
+ * @brief  
+ * @version 1
+ * @date 2020-4-23
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#ifndef UDS_ReadDTCInformation_INTERFACE_H
+#define UDS_ReadDTCInformation_INTERFACE_H
+
+
+#endif
---
+++ System/04-SERVICE/UDS Services/ReadDTCInformation/UDS_ReadDTCInformation_private.h
@@ -0,0 +1,13 @@
+/**
+ * @file UDS_ReadDTCInformation_private.h
+ * @author Ahmed M.Abdelfattah (ahmedm7md24@gmail.com)
+ * @brief  
+ * @version 1
+ * @date 2020-4-23
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#ifndef UDS_ReadDTCInformation_PRIVATE_H
+#define UDS_ReadDTCInformation_PRIVATE_H
+#endif
