0412e762d42141db5491bee03560670431444431
Ah2m4ed <40804198+Ah2m4ed@users.noreply.github.com>
2021-05-25 16:08:18 +0200

    DTC generation code

---
+++ System/04-SERVICE/DTC/DTC_private.h
@@ -0,0 +1,19 @@
+/**
+ * @file DTC_private.h
+ * @author Ahmed M.Abdelfattah (ahmedm7md24@gmail.com)
+ * @brief  
+ * @version 1
+ * @date 2021-5-17
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef DTC_PRIVATE_H
+#define DTC_PRIVATE_H
+
+void dtcSetFault(dtcItem_t* it, dtcEnvironment_t* env);
+
+void dtcSetPassed(dtcItem_t* it, dtcEnvironment_t* env);
+
+
+#endif
