1c65b98506a69a946a858e09e219c4cb407b6e96
Adham Eid <adhameid0@gmail.com>
2021-05-30 23:47:26 +0200

    PARSE driver

---
+++ System/05-APP/Parse/PARSE_interface.h
@@ -0,0 +1,17 @@
+/**
+ * @file PARSE_interface.h
+ * @author Adham Eid (adhameid0@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 30-05-2020
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#ifndef PARSE_INTERFACE_H_
+#define PARSE_INTERFACE_H_
+
+void PARSE_voidFlashRecord(u8* Copy_au8Record);
+
+#endif
