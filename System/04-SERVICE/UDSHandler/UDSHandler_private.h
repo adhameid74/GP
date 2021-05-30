20c8d00807b407f6c080c9a06b886b5c8f0de250
Adham Eid <adhameid0@gmail.com>
2021-05-30 13:55:21 +0200

    UDS handler update

--- System/04-SERVICE/UDSHandler/UDSHandler_private.h
+++ System/04-SERVICE/UDSHandler/UDSHandler_private.h
@@ -12,16 +12,6 @@
 #ifndef UDSHANDLER_PRIVATE_H_
 #define UDSHANDLER_PRIVATE_H_
 
-#define DiagnosticSessionControl				0x10
-#define ClearDiagnosticInformation				0x14
-#define ReadDTCInformation						0x19
-#define ReadDataByIdentifier					0x22
-#define WriteDataByIdentifier					0x2E
-#define RequestDownload							0x34
-#define TransferData							0x36
-#define RequestTransferExit						0x37
-#define TesterPresent							0x3E
-
 #define NRC_LENGTH								3
 #define NRC_ID									0x7F
 
