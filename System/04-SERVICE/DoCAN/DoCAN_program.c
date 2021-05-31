e456bd834aa72bc01fc9bf7665e811b5b26d9547
Hendawy98 <ahmed.ali982@eng-st.cu.edu.eg>
2021-05-31 22:31:44 +0200

    update DoCAN

--- System/04-SERVICE/DoCAN/DoCAN_program.c
+++ System/04-SERVICE/DoCAN/DoCAN_program.c
@@ -16,9 +16,9 @@
  #include "DoCAN_interface.h"
  #include "DoCAN_private.h"
  #include "DoCAN_config.h"
- void (*MassageIndication)(void);
+ void (*MassageIndication)(INDICATION_SDU SDU);
  void (*FlowControlIndication)(void);
- void DoCAN_voidSetCallBackForMassage(void (*FPtr)(void)){
+ void DoCAN_voidSetCallBackForMassage(void (*FPtr)(INDICATION_SDU SDU)){
 	 MassageIndication = FPtr ;
  }
  
@@ -30,12 +30,12 @@
 	 CAN_msg_t Frame ;
 	 TX_STATE_t State ;
 	 Frame.ID = SOURCE_ADDRESS ;
-	 if(SDU.Length <= 7){
+	 if(SDU.Length.u12 <= 7){
 		 /*Construct Single Frame */
-		 Frame.DLC =(u8)(SDU.Length &0x007) ;
+		 Frame.DLC =(u8)SDU.Length.u12 ;
 		 Frame.DATA[0] = Frame.DLC ;
 		 Frame.DLC++ ;
-		 for(i = 0 ; i < SDU.Length ;i++){
+		 for(i = 0 ; i < SDU.Length.u12 ;i++){
 			 Frame.DATA[i+1] = SDU.MessageData[i];
 		 }
 		 /*Send Frame */
@@ -43,19 +43,19 @@
 	 }
 	 else {
 		 /*Construct First Frame */
-		 Frame.DATA[0] = 0x10 |((u8)(SDU.Length >> 8));
-		 Frame.DATA[1] = (u8)SDU.Length ;
+		 Frame.DATA[0] = 0x10 |((u8)(SDU.Length.u12 >> 8));
+		 Frame.DATA[1] = (u8)SDU.Length.u12 ;
 		 for(i = 1 ; i <= 6 ;i++){
 			 Frame.DATA[i+1] = SDU.MessageData[i-1];
 		 }
 		 Frame.DLC = 8 ; 
 		 State=CAN_u8WriteMsg(&Frame);
+		 //if(State = CAN_TX_OK);
 		 /*Construct Consective Frames */
 		 u8 SeqNumber = 1 ;
-		 NumByteLastCf = SDU.Length % 8;
-		 for (i--; i< SDU.Length ;){
+		 for (i--; i< SDU.Length.u12 ;){
 			 Frame.DATA[0] = 0x20 | SeqNumber ;
-			 for(j=0; j < 7 && i < SDU.Length ; i++,j++){
+			 for(j=0; j < 7 && i < SDU.Length.u12 ; i++,j++){
 				 Frame.DATA[j+1] = SDU.MessageData[i];
 			 }
 			 if(j < 7)
@@ -69,23 +69,25 @@
 	 }
  }
   void DoCAN_voidIndicateUsData(CAN_msg_t CAN_Message){
-	  static u16 i ;
+	  u16 i ;
 	  u16 j ;
-      static u8 Expected_SN = 1 ;	  
+	  static u12 Length  ;
+	  static u12 Received_Data_length ={0};
+      u8 Current_SN ;
+	  u8 Frame_Data_Length ; 
 	  if(CAN_Message.ID == SOURCE_ADDRESS ){
 		  //Get PCI to know Frame Type 
-		  u8 PciType = CAN_Message.Data[0] >> 4 ;
+		  u8 PciType = CAN_Message.DATA[0] >> 4 ;
 		  Received_Data.SA = CAN_Message.ID ;
 		  Received_Data.TA = TARGET_ADDRESS ;
 		  if(PciType == 0){
 			  //Single Frame 
-			  
-			  if(Received_Data.Length >= 8){
-				  Received_Data.Length = CAN_Message.DLC - 1;
-				  for(i=0 ; i < Received_Data.Length ; i++ ){
+			  Received_Data.Length.u12 = CAN_Message.DLC - 1;
+			  if(Received_Data.Length.u12 <= 7){
+				  for(i=0 ; i < Received_Data.Length.u12 ; i++ ){
 					  Received_Data.MessageData[i] = CAN_Message.DATA[i+1] ;
 				  }
-				  MassageIndication();
+				  MassageIndication(Received_Data);
 			  }
 			  else{
 				  //Erorr
@@ -93,38 +95,56 @@
 		  }
 		  else if (PciType == 1){
 			  //First Frame 
-			  Received_Data.Length =  (u12)(((CAN_Message.DATA[0] << 8) 0x0F) | CAN_Message.DATA[1]) ;
-			  //check Length > 7 or not 
+			  Received_Data.Length.u12  = 0 ;
+			  Received_Data.Length.u12  = (CAN_Message.DATA[0] & 0X0F) << 4 ;
+			  Received_Data.Length.u12 |= CAN_Message.DATA[0] ;
+			  Length.u12 = Received_Data.Length.u12  ;
+			  //check Length.u12 > 7 or not 
 			  for(i = 0 ; i < 6 ; i++){
 				 Received_Data.MessageData[i] = CAN_Message.DATA[i+2] ; 
 			  }
-			  Received_Data.Length -= 6 ; 
+			  Received_Data_length.u12 += 6; 
 			  
 		  }
 		  else if (PciType == 2){
 			  //Consective Frame 
-			  if(Expected_SN == CAN_Message.DATA[0] & 0x0F){
+			  Current_SN = CAN_Message.DATA[0] & 0x0F ; 
+			  Frame_Data_Length = CAN_Message.DLC - 1 ;
+			  i = (Current_SN -1 ) * 7 + 6 ; 
+			  j = 1 ; 
+			  for(; i < i+Frame_Data_Length; i++){
+				  Received_Data.MessageData [i]= CAN_Message.DATA[j++];
+				  
+			  }
+			  Received_Data_length.u12 += Frame_Data_Length; 
+			  if (Received_Data_length.u12 == Length.u12 ){
+				  Received_Data_length.u12 = 0 ; 
+				  MassageIndication(Received_Data);
+			  }
+			  
+			  
+			  /*if(Expected_SN == CAN_Message.DATA[0] & 0x0F){
 				  Expected_SN ++ ;
 				  if(Expected_SN == 15 )
 					  Expected_SN = 0 ;
-				  if(Received_Data.Length > 8){
+				  if(Length.u12 > 8){
 					  for (j = 1; j < 8;i++ , j++){
 						  Received_Data.MessageData[i] = CAN_Message.DATA[j] ;
 					  }
-					  Received_Data.Length -= 8 ;
+					  Received_Data.Length.u12 -= 8 ;
 				  }
 				  else {
-					  for(j = 1 ;j <= Received_Data.Length ; j++ ,i++ ){
+					  for(j = 1 ;j <= Length.u12 ; j++ ,i++ ){
 						  Received_Data.MessageData[i] = CAN_Message.DATA[j] ;
 					  }
-					  MassageIndication();
+					  MassageIndication(Received_Data);
 					  i = 0;
 				  }
 			  }
 			  else {
 				  //Erorr unExpectedSN
 			  }
-		  }
+		  }*/
 		  else if (PciType == 3){
 			  //Flow Control Frame 
 			  Received_FlowControl.FlowStatus = CAN_Message.DATA[0] & 0x0F ;
@@ -138,3 +158,4 @@
 	  }
 	  
   }
+}
