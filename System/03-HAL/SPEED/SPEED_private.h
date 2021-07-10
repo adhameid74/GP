df0e5699e13338615b82d621de241e05f67aeefa
Adham Eid <adhameid0@gmail.com>
2021-07-10 20:01:21 +0200

    Speed DTCs

--- System/03-HAL/SPEED/SPEED_private.h
+++ System/03-HAL/SPEED/SPEED_private.h
@@ -11,6 +11,17 @@
 #ifndef SPEED_PRIVATE_H
 #define SPEED_PRIVATE_H
 
+static u8 Flag = 0;
 
+#define DTCNUM                     3
+
+#define FAILED_THRESHOLD           25
+#define PASSED_THRESHOLD           -10
+#define AGING_THRESHOLD            255
+
+#define SPEED_LOW_THRESHOLD        50
+#define SPEED_LIMIT                125
+
+static dtcItem_t SPEED_DTC[DTCNUM];
 
 #endif
--- System/03-HAL/SPEED/SPEED_program.c
+++ System/03-HAL/SPEED/SPEED_program.c
@@ -13,17 +13,53 @@
 
 #include "TIMER_interface.h"
 
+#include "DTC_interface.h"
+
+#include "CAR_interface.h"
+
 #include "SPEED_interface.h"
 #include "SPEED_private.h"
 #include "SPEED_config.h"
+
 u16 SPEED_u16Reading = 0;
 
 void SPEED_voidGetReadingAsynch()
 {
+	u16 Local_u16ExpectedReading;
+	if(Flag == 0)
+	{
+		for (u8 Local_u8Counter = 0; Local_u8Counter < DTCNUM; Local_u8Counter++)
+		{
+			SPEED_DTC[Local_u8Counter].Property->Code = DTC_SpeedNotMatched + Local_u8Counter;
+			SPEED_DTC[Local_u8Counter].Property->TestFailedThreshold = FAILED_THRESHOLD;
+			SPEED_DTC[Local_u8Counter].Property->TestPassedThreshold = PASSED_THRESHOLD;
+			SPEED_DTC[Local_u8Counter].Property->AgingThreshold = AGING_THRESHOLD;
+		}
+		Flag = 1;
+	}
+
 	SPEED_u16Reading = TIMER_u16GetElapsedTime(TIMER_ID);
 	SPEED_u16Reading = ( (SPEED_u16Reading * 1000) / (NUM_OF_HOLES * EVALUATION_TIME) );
 	TIMER_voidSetResetTimer(TIMER_ID, TIMER_RESET, 0);
 	TIMER_voidEnableExternalClockSource(TIMER_ID, ((TIMER_ID*10)+22));
+
+	if( (CAR_SPEED_CurrentValue > SPEED_LOW_THRESHOLD) && (SPEED_u16Reading == 0) )
+	{
+		DTC_u8DetectFault(&SPEED_DTC[2], 1);
+		return;
+	}
+	DTC_u8DetectFault(&SPEED_DTC[2], 0);
+
+	Local_u16ExpectedReading = CAR_SPEED_CurrentValue*12; // Habd equation
+	if ( (SPEED_u16Reading > (Local_u16ExpectedReading+20)) || (SPEED_u16Reading < (Local_u16ExpectedReading-20)) )
+	{
+		DTC_u8DetectFault(&SPEED_DTC[0], 1);
+	}
+	DTC_u8DetectFault(&SPEED_DTC[0], 0);
+
+	if ( SPEED_u16Reading > SPEED_LIMIT )
+	{
+		DTC_u8DetectFault(&SPEED_DTC[1], 1);
+	}
+	DTC_u8DetectFault(&SPEED_DTC[1], 0);
 }
-
-
